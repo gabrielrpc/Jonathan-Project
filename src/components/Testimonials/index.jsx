a7b431207b27964f08e3aed0ff66ae9bdcdad46b
Gabriel Carvalho <grpc15@outlook.com>
2024-02-27 17:55:39 -0300

    correção nas imagens do testimonials

--- src/components/Testimonials/index.jsx
+++ src/components/Testimonials/index.jsx
@@ -3,10 +3,6 @@ import { Carousel, Col, Container, Row } from 'react-bootstrap'
 import styles from './projects.module.css'
 import Image from 'next/image'
 import { useState } from 'react'
-import liam from '../../../public/liam.png'
-/* import emma_cliente from '../../../public/emma_cliente.png' */
-import manson from '../../../public/manson.png'
-
 
 export default function Testimonials() {
 
@@ -26,24 +22,24 @@ export default function Testimonials() {
 
                 <Col xs={10} lg={5} className={styles.text_content}>
                     <Carousel activeIndex={index} onSelect={handleSelect} className='carousel-testimonials'>
-                        {/* <Carousel.Item>
+                        <Carousel.Item>
                             <p>
-                                &quot; I couldn&rsquo;t be happier with the new flooring on my balcony! The team did an amazing job, and the results exceeded my expectations. Now I can enjoy my morning coffee outside in style. Thank you for the excellent service!&quot;
+                                &quot;I couldn&rsquo;t be happier with the new flooring on my balcony! The team did an amazing job, and the results exceeded my expectations. Now I can enjoy my morning coffee outside in style. Thank you for the excellent service!&quot;
                             </p>
                             <div className={styles.person}>
-                                <Image src={emma_cliente} className={styles.image} alt='Delighted Customer' />
+                                <Image src="/emma.png" className={styles.image} alt='Satisfied Client' width={70} height={70} />
                                 <div>
                                     <span>Emma Smith</span>
                                     <p>Delighted Customer</p>
                                 </div>
                             </div>
-                        </Carousel.Item> */}
+                        </Carousel.Item>
                         <Carousel.Item>
                             <p>
                                 &quot;I couldn&rsquo;t be happier with the outcome of my bathroom flooring renovation! The tiles look fantastic, and the installation process was seamless. It&rsquo;s like having a brand new bathroom. Highly recommend their services!&quot;
                             </p>
                             <div className={styles.person}>
-                                <Image src={liam} className={styles.image} alt='Satisfied Client' />
+                                <Image src="/liam.png" className={styles.image} alt='Satisfied Client' width={70} height={70} />
                                 <div>
                                     <span>Liam J.</span>
                                     <p>Satisfied Client</p>
@@ -55,7 +51,7 @@ export default function Testimonials() {
                                 &quot;I&rsquo;m so impressed  the professionalism and expertise of the team that replaced the flooring in my bathroom. The attention to detail is remarkable, and the end result is beyond my expectations. Thank you for a job well done!&quot;
                             </p>
                             <div className={styles.person}>
-                                <Image src={manson} className={styles.image} alt='Happy Customer' />
+                                <Image src='/manson.png' className={styles.image} alt='Happy Customer' width={70} height={70} />
                                 <div>
                                     <span>Mason B.</span>
                                     <p>Happy Customer</p>
