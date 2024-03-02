6f188a3a2670ff938649e53f7003e754b5b0a35f
Gabriel Carvalho <grpc15@outlook.com>
2024-03-02 08:14:08 -0300

    Ajuste no estilo de projects

---
+++ src/components/Projects/images.js
@@ -0,0 +1,26 @@
+import slide_image from '../../../public/slide_image.jpg'
+import slide_image1 from '../../../public/slide_image1.jpg'
+import slide_image2 from '../../../public/slide_image2.jpg'
+import slide_image3 from '../../../public/slide_image3.jpg'
+import slide_image4 from '../../../public/slide_image4.jpg'
+import slide_image5 from '../../../public/slide_image5.jpg'
+import slide_image6 from '../../../public/slide_image6.jpg'
+import slide_image7 from '../../../public/slide_image7.jpg'
+import slide_image8 from '../../../public/slide_image8.jpg'
+import slide_image9 from '../../../public/slide_image9.jpg'
+import slide_image10 from '../../../public/slide_image10.jpg'
+import slide_image11 from '../../../public/slide_image11.jpg'
+import slide_image12 from '../../../public/slide_image12.jpg'
+
+
+export const images = [
+    { src: slide_image6, alt: 'image_slide_6' },
+    { src: slide_image12, alt: 'image_slide_12' },
+    { src: slide_image7, alt: 'image_slide_7' },
+    { src: slide_image9, alt: 'image_slide_9' },
+    { src: slide_image10, alt: 'image_slide_10' },
+    { src: slide_image11, alt: 'image_slide_11' },
+    { src: slide_image3, alt: 'image_slide_3' },
+    { src: slide_image5, alt: 'image_slide_5' },
+    { src: slide_image1, alt: 'image_slide_1' },
+]
--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -1,57 +1,84 @@
 "use client"
-import { Carousel, Col, Container, Row } from 'react-bootstrap'
+import { Col, Container, Row } from 'react-bootstrap'
 import styles from './projects.module.css'
-import Image from 'next/image'
-import slide from '../../../public/slide_image.jpg'
-import slide1 from '../../../public/slide_image1.jpg'
-import slide2 from '../../../public/slide_image2.jpg'
+
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { FreeMode, Navigation, Thumbs } from 'swiper/modules'
+import { images } from './images'
 import { useState } from 'react'
+import Image from 'next/image'
+
+import 'swiper/css'
+import 'swiper/css/free-mode'
+import 'swiper/css/navigation'
+import 'swiper/css/thumbs'
 
 
 export default function Projects() {
 
-    const [index, setIndex] = useState(0);
-
-    const handleSelect = (selectedIndex) => {
-        setIndex(selectedIndex);
-    };
+    const [thumbsSwiper, setThumbsSwiper] = useState(null)
 
     return (
-        <Container id='projects' fluid className={`${styles.projects} bg-degrade`}>
+        <Container id='projects'>
             <Row className={styles.content_projects}>
                 <Col xs={12} className={styles.projects_text}>
                     <h2><span>Recently</span> projects</h2>
                     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. In officia dignissimos perspiciatis?</p>
                 </Col>
 
-                <Col xs={12} lg={9}>
-                    <Carousel activeIndex={index} onSelect={handleSelect}>
-                        <Carousel.Item>
-                            <div className="d-flex align-items-center justify-content-center">
-                                <Image className={styles.image_principal} src={slide} alt='trabalhadores' />
-                            </div>
-                        </Carousel.Item>
-                        <Carousel.Item>
-                            <div className="d-flex align-items-center justify-content-center">
-                                <Image className={styles.image_principal} src={slide1} alt='trabalhadores' />
-                            </div>
-                        </Carousel.Item>
-                        <Carousel.Item>
-                            <div className="d-flex align-items-center justify-content-center">
-                                <Image className={styles.image_principal} src={slide2} alt='trabalhadores' />
-                            </div>
-                        </Carousel.Item>
-                    </Carousel>
+                <Col lg={{span: 8, offset: 2}}>
+                    <Swiper
+                        loop={true}
+                        spaceBetween={10}
+                        navigation={true}
+                        thumbs={{
+                            swiper: thumbsSwiper && !thumbsSwiper.destroyed ? thumbsSwiper : null
+                        }}
+                        modules={[FreeMode, Navigation, Thumbs]}
+                        className={styles.carousel_principal}
+                    >
+                        {images.map((image, index) => {
+                            return (<SwiperSlide key={index} >
+                                <div className={styles.content_image_principal}>
+                                    <Image
+                                        src={image.src}
+                                        alt={image.alt}
+                                        className={styles.image_principal}
+                                    />
+                                </div>
+                            </SwiperSlide>)
+                        })}
+                    </Swiper>
+                </Col>
+
+                <Col lg={{span: 8, offset: 2}} className='d-none d-md-block'>
+                    <Swiper
+                        onSwiper={setThumbsSwiper}
+                        loop={true}
+                        spaceBetween={12}
+                        slidesPerView={6}
+                        freeMode={true}
+                        watchSlidesProgress={true}
+                        modules={[FreeMode, Navigation, Thumbs]}
+                        className={styles.thumbs + ' img-thumbnail'}
+                    >
+                        {images.map((image, index) => {
+                            return (
+                                <SwiperSlide key={index}>
+                                    <button className={styles.button_slide}>
+                                        <Image
+                                            src={image.src}
+                                            alt={image.alt}
+                                            className={styles.thumbs_image}
+                                        />
+                                    </button>
+                                </SwiperSlide>
+                            )
+                        })}
+
+                    </Swiper>
                 </Col>
             </Row>
-
-            {/*                 <Col className={styles.content_images_display}>
-                    <Image className={styles.image_slide} src={trabalhadores} alt='trabalhadores' />
-                    <Image className={styles.image_slide} src={trabalhadores} alt='trabalhadores' />
-                    <Image className={styles.image_slide} src={trabalhadores} alt='trabalhadores' />
-                    <Image className={styles.image_slide} src={trabalhadores} alt='trabalhadores' />
-                </Col> */}
-
         </Container>
     )
 }
